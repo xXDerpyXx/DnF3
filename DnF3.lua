d0222a8e1844896cc83d7468e8747c3700962198
Derpy <derpy@CrapTopV2.home>
2017-03-16 22:08:20 -0400

    Just something to work with

--- DnF3.lua
+++ DnF3.lua
@@ -1,1 +1,21 @@
-crap
+--[[
+DICTATOR ADVISOR STAT EXAMPLE
+(probably will be loaded from a file in the future)
+list = {
+	ID = {
+		name="name",
+		happinessModifier=5,
+		loyaltyModifier=5,
+		otherDictatorLoyaltyModifier=5,
+		moneyAddedPerTurn=5,
+		taxModifier=0,
+		socialSpendingModifier=0
+	}
+}
+
+]]--
+
+tax = 15
+socialSpending = 250
+population = 1000
+money = 1000
